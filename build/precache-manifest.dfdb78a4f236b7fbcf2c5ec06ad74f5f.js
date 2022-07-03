ba4251a71bda9a92e233fb6a5dfe0c0b67c026b2
Kyle Glasper <ksglasper@gmail.com>
2022-07-03 00:35:58 -0500

    created build folder

---
+++ build/precache-manifest.dfdb78a4f236b7fbcf2c5ec06ad74f5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0644904df399a7ca4e2e17a5143ce167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adb3a1cab9e6012fcc7",
+    "url": "/static/js/2.6c99d600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c99d600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db31252e7cc57197400d",
+    "url": "/static/js/main.d71ee2c2.chunk.js"
+  },
+  {
+    "revision": "aa7c612222590812d412",
+    "url": "/static/js/runtime-main.1160daa0.js"
+  }
+]);
